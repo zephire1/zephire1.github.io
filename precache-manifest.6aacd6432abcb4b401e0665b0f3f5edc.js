87f3dad10ea4c2d1ff25a1f1900873317e5a3ae6
zephire1 <rredgame@bk.ru>
2019-08-08 13:59:01 +0300

    fix

--- precache-manifest.6aacd6432abcb4b401e0665b0f3f5edc.js
+++ precache-manifest.6aacd6432abcb4b401e0665b0f3f5edc.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "8623a5226f2b490b89c6",
-    "url": "/static/js/main.e62a9a05.chunk.js"
+    "revision": "ce5b8e7643673653cbe0",
+    "url": "/static/js/main.ccbaa699.chunk.js"
   },
   {
     "revision": "652bc9982b7dda23075c",
     "url": "/static/js/2.43db5626.chunk.js"
   },
   {
-    "revision": "8623a5226f2b490b89c6",
+    "revision": "ce5b8e7643673653cbe0",
     "url": "/static/css/main.ca9a6b21.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/2.73f9868b.chunk.css"
   },
   {
-    "revision": "57aae7333f3ad9c7562def7a269fbbf3",
+    "revision": "34af0f3ba5be088bd87144c409475203",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b028f61c422becdd9e63ceb3c343d099.js"
+  "/precache-manifest.6aacd6432abcb4b401e0665b0f3f5edc.js"
 );
 
 workbox.clientsClaim();
