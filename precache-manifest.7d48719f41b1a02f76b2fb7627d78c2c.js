ebcd663922a42d6021df87ffcc9fad2e22893d66
zephire1 <rredgame@bk.ru>
2019-08-08 14:41:26 +0300

    fix

--- precache-manifest.7d48719f41b1a02f76b2fb7627d78c2c.js
+++ precache-manifest.7d48719f41b1a02f76b2fb7627d78c2c.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "9b5ad9e32a3bb4fe003b",
-    "url": "/static/js/main.b15f2cee.chunk.js"
+    "revision": "ef721444489c25c226f8",
+    "url": "/static/js/main.91572425.chunk.js"
   },
   {
     "revision": "652bc9982b7dda23075c",
     "url": "/static/js/2.43db5626.chunk.js"
   },
   {
-    "revision": "9b5ad9e32a3bb4fe003b",
+    "revision": "ef721444489c25c226f8",
     "url": "/static/css/main.ca9a6b21.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/2.73f9868b.chunk.css"
   },
   {
-    "revision": "fdf66e0d566623ed0417838e3bd01a80",
+    "revision": "53df315ba845aa67436519a41644889f",
     "url": "/index.html"
   }
 ];
