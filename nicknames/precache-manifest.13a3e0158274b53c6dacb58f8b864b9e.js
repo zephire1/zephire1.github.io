e90cbf7bf5d3cf433b0691add543afd3740dd4cb
zephire1 <rredgame@bk.ru>
2019-09-23 22:34:55 +0300

    яяя

---
+++ nicknames/precache-manifest.13a3e0158274b53c6dacb58f8b864b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fffe5ce51114e43fa57",
+    "url": "/nicknames/static/js/runtime~main.3b8f854b.js"
+  },
+  {
+    "revision": "d1800b9a4da7c4662ee8",
+    "url": "/nicknames/static/js/main.e1b6d547.chunk.js"
+  },
+  {
+    "revision": "11212f7b30d18b1e47f5",
+    "url": "/nicknames/static/js/2.94d3b3b7.chunk.js"
+  },
+  {
+    "revision": "d1800b9a4da7c4662ee8",
+    "url": "/nicknames/static/css/main.463af905.chunk.css"
+  },
+  {
+    "revision": "11212f7b30d18b1e47f5",
+    "url": "/nicknames/static/css/2.4c2f670c.chunk.css"
+  },
+  {
+    "revision": "f33f66368f5706bfe4b91c6f744c5dda",
+    "url": "/nicknames/index.html"
+  }
+];
