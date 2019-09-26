38fc1d13f3bd499f5c9554e20c9d1bb35d3df186
zephire1 <rredgame@bk.ru>
2019-09-26 15:22:06 +0300

    чч

---
+++ rifma4/precache-manifest.fc418ca44c6a8309462c4c4de139bcf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/rifma4/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "5688ae17367fb842215d",
+    "url": "/rifma4/static/js/runtime~main.e162ade0.js"
+  },
+  {
+    "revision": "2717d8b7ad6e46ecbf0e",
+    "url": "/rifma4/static/js/main.631bafc6.chunk.js"
+  },
+  {
+    "revision": "361e3761e81afd8cfc1c",
+    "url": "/rifma4/static/js/2.19faa8cc.chunk.js"
+  },
+  {
+    "revision": "2717d8b7ad6e46ecbf0e",
+    "url": "/rifma4/static/css/main.4f7532c9.chunk.css"
+  },
+  {
+    "revision": "361e3761e81afd8cfc1c",
+    "url": "/rifma4/static/css/2.4c2f670c.chunk.css"
+  },
+  {
+    "revision": "9ac504d47aa7416daecda922b59bde7a",
+    "url": "/rifma4/index.html"
+  }
+];
